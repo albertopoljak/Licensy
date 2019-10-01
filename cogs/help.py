dc6ad5a0ba6eec8efe150f2ce89654aa90cc91d0
Braindead <albertopoljak@gmail.com>
2019-10-01 04:52:22 +0200

    Help command DM fix

--- cogs/help.py
+++ cogs/help.py
@@ -28,9 +28,17 @@ class PrettyHelpCommand(commands.MinimalHelpCommand):
             self.paginator.add_line(joined)
 
     async def send_pages(self):
+        empty = discord.Embed.Empty
         destination = self.get_destination()
+
+        try:
+            color = self.context.me.top_role.color
+        except AttributeError:
+            # Fix for DMs
+            color = empty
+
         for page in self.paginator.pages:
-            embed = discord.Embed(title=discord.Embed.Empty, description=page, color=self.context.me.top_role.color)
+            embed = discord.Embed(title=empty, description=page, color=color)
             await destination.send(embed=embed)
 
 
