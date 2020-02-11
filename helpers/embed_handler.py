ecc4d2ea861f2cf630d4292db79aef72e68aa7b6
Braindead <albertopoljak@gmail.com>
2020-02-11 17:22:44 +0100

    Refactored bot.py, better logging
    Events are now inside the bot subclass.
    Bot will run only if the script is ran not imported.
    Better logging.

--- helpers/embed_handler.py
+++ helpers/embed_handler.py
@@ -10,13 +10,12 @@ def simple_embed(message: str, title: str, color: Colour) -> Embed:
 
 def info(message: str, member: Union[Member, User, None], title: str = Embed.Empty) -> Embed:
     """
-    Constructs success embed with custom title and description.
+    Constructs info embed with custom title and description.
     Color depends on passed member top role color.
     :param message: embed description
-    :param member: member object to get the color of it's top role from
+    :param member: member/user object to get the color of it's top role from
     :param title: title of embed
     :return: Embed object
-
     """
     embed = Embed(title=title, description=message, color=misc.get_top_role_color(member))
     return embed
@@ -26,13 +25,10 @@ def success(message: str, member: Union[Member, User, None]) -> Embed:
     """
     Constructs success embed with fixed title:Success and color depending
     on passed member top role color.
-    This will be used quite common so no sense to hard-code green colour since
-    we want most of the messages the bot sends to be the color of it's top role.
     :param message: embed description
     :param member: member object to get the color of it's top role from,
                    usually our bot member object from the specific guild.
     :return: Embed object
-
     """
     return simple_embed(message, "Success", misc.get_top_role_color(member))
 
@@ -42,7 +38,6 @@ def warning(message: str) -> Embed:
     Constructs warning embed with fixed title:Warning and color:gold
     :param message: embed description
     :return: Embed object
-
     """
     return simple_embed(message, "Warning", Colour.dark_gold())
 
@@ -52,26 +47,5 @@ def failure(message: str) -> Embed:
     Constructs failure embed with fixed title:Failure and color:red
     :param message: embed description
     :return: Embed object
-
     """
     return simple_embed(message, "Failure", Colour.red())
-
-
-def log_embed(*messages, ctx=None, title="Log"):
-    embed = Embed(title=title)
-    for index, message in enumerate(messages):
-        embed.add_field(name=f"Message {index+1}:", value=message, inline=True)
-    if ctx is not None:
-        guild = "DM" if ctx.guild is None else ctx.guild.id
-        footer = f"Guild: {guild}    Author: {ctx.author}"
-        embed.set_footer(text=footer)
-    return embed
-
-
-def traceback_embed(traceback_last):
-    # Allow passing both list and string
-    if type(traceback_last) is list:
-        traceback_last = " ".join(traceback_last)
-    description = misc.maximize_size(traceback_last)
-    embed = Embed(title="Traceback", description=description)
-    return embed
