5760beeb2a6d16cb730eeda4b034275f5229b88a
Braindead <albertopoljak@gmail.com>
2020-02-11 17:23:40 +0100

    Small refactor of config_handler
    Now uses "private" attributes.
    Added repr.

--- config_handler.py
+++ config_handler.py
@@ -6,14 +6,21 @@ logger = logging.getLogger(__name__)
 
 
 class ConfigHandler:
+    """
+    Class that handles interaction with config json files.
+    Values can be accessed with bracket notation self[key]
+    """
     CONFIG_DIR = Path("")
 
     def __init__(self, config_name: str):
         """
         :param config_name: name of the config file without the suffix.
         """
-        self.path = ConfigHandler.CONFIG_DIR / (config_name + ".json")
-        self.config = self._load_config()
+        self._path = ConfigHandler.CONFIG_DIR / (config_name + ".json")
+        self._config = self._load_config()
+
+    def __repr__(self):
+        return f"{json.dumps(self._config, indent=4, sort_keys=True)}"
 
     def _load_config(self) -> dict:
         """
@@ -21,11 +28,11 @@ class ConfigHandler:
         :return: dict loaded json data
         """
         try:
-            with open(self.path) as cfg:
+            with open(self._path) as cfg:
                 data = json.load(cfg)
                 return data
         except FileNotFoundError as e:
-            logger.critical(f"Config json file was not found: {self.path} : {e}")
+            logger.critical(f"Config json file was not found: {self._path} : {e}")
         except ValueError as e:
             logger.critical(f"Invalid config json: {e}")
         except KeyError as e:
@@ -39,24 +46,24 @@ class ConfigHandler:
         If you change the config manually while the bot is running you need to call this method
         so the values are updated in memory.
         """
-        self.config = self._load_config()
+        self._config = self._load_config()
 
     def __getitem__(self, key: str):
         return self._get_key(key)
 
     def _get_key(self, key: str):
         try:
-            return self.config[key]
+            return self._config[key]
         except KeyError as e:
             error_message = f"Key '{key}' not found in json config! {e}"
             logger.critical(error_message)
             raise KeyError(error_message)
 
-    def update_key(self, key, value):
+    def update_key(self, key: str, value):
         try:
-            self.config[key] = value
-            with open(self.path, "w") as cfg:
-                json.dump(self.config, cfg, indent=4, sort_keys=True)
+            self._config[key] = value
+            with open(self._path, "w") as cfg:
+                json.dump(self._config, cfg, indent=4, sort_keys=True)
         except TypeError as e:
             logger.critical(f"Unable to serialize the object {e}")
         except Exception as e:
